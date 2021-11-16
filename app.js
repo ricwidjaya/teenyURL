78c9aa62d77588a71b4c75a0b56056f0471b15aa
Richard Widjaya <ricwidjaya@gmail.com>
2021-11-16 20:09:32 +0800

    feat: add body parser

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ require('./config/mongoose')
 // Static files
 app.use(express.static('public'))
 
+// Body parser
+app.use(express.urlencoded({ extended: true }))
+
 // Set template engine to handlebars
 app.engine('handlebars', exhbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -20,6 +23,11 @@ app.set('view engine', 'handlebars')
 // Routers
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  console.log('post request')
 })
 
 app.listen(PORT, () => {

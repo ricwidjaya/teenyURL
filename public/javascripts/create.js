1a6fdfa59e0c7789132fc3d401aab66d6c05864d
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-13 20:00:10 +0800

    feat: add UTM validation

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,11 +1,9 @@
-const copyBtn = document.querySelector('#copyBtn')
-
+const copyBtn = document.querySelector("#copyBtn")
 
 // Listen to copy buttons clicks
-copyBtn.addEventListener('click', (event) => {
+copyBtn.addEventListener("click", (event) => {
   const target = event.target
   console.log(target)
-  const url = 'localhost:3000/teenyurl/' + target.dataset.url
+  const url = "localhost:3000/teenyurl/" + target.dataset.url
   navigator.clipboard.writeText(url)
-
 })
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,53 @@
-const view = document.querySelectorAll('.view')
-const form = document.querySelector('#form')
-const urlInput = document.querySelector('#url')
-const alert = document.querySelector('.alert')
-const submitBtn = document.querySelector('#submitBtn')
+const view = document.querySelectorAll(".view")
+const form = document.querySelector("#form")
+const urlInput = document.querySelector("#url")
+const urlAlert = document.querySelector("#invalid-url")
+const utmAlert = document.querySelector("#invalid-utm")
+const submitBtn = document.querySelector("#submitBtn")
 
+// UTMs
+const source = document.querySelector("#source")
+const medium = document.querySelector("#medium")
+const campaign = document.querySelector("#campaign")
 
 // Listen to copy buttons clicks
-view.forEach(link => {
-  link.addEventListener('click', (event) => {
+view.forEach((link) => {
+  link.addEventListener("click", (event) => {
     const target = event.target
-    if (target.tagName === 'BUTTON') {
-      const url = 'localhost:3000/teenyurl/' + target.dataset.url
+    if (target.tagName === "BUTTON") {
+      const url = "localhost:3000/teenyurl/" + target.dataset.url
       navigator.clipboard.writeText(url)
     }
   })
 })
 
-
 // Check url validation
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   const url = urlInput.value
   if (!validateURL(url)) {
     event.preventDefault()
-    alert.classList.remove('fadeout')
-    setTimeout(() => { alert.classList.add('fadeout') }, 200)
+    urlAlert.classList.remove("fadeout")
+    setTimeout(() => {
+      urlAlert.classList.add("fadeout")
+    }, 200)
+  }
+})
+
+// UTM validation
+form.addEventListener("submit", (event) => {
+  const srcValue = source.value
+  const medValue = medium.value
+  const camValue = campaign.value
+  if (
+    (srcValue && medValue && camValue) ||
+    (!srcValue && !medValue && !camValue)
+  ) {
+  } else {
+    event.preventDefault()
+    utmAlert.classList.remove("fadeout")
+    setTimeout(() => {
+      utmAlert.classList.add("fadeout")
+    }, 200)
   }
 })
 
@@ -39,5 +63,5 @@ function validateURL(str) {
     console.log(error)
     return false
   }
-  return url.protocol === 'https:' || url.protocol === 'http:'
+  return url.protocol === "https:" || url.protocol === "http:"
 }
